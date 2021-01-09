9f0f6963f36dcf275d26366f7789c28a2c2790b9
工藤千寛 <kudousenhiroshi@ChihiroKudo.local>
2021-01-10 02:25:31 +0900

    add files of counting

---
+++ auditlog_counter.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 512
+
+int main() {
+
+    char *target1 = "-A--";
+    char *target2 = "-Z--";
+    char *adr1;
+    char *adr2;
+    int counter1 = 0;
+    int counter2 = 0;
+    char str_buf[BUFSIZE];
+
+    //ファイル構造体へのポインタを宣言
+    FILE *fp;
+
+    printf("[TARGET STRING : %s %s]\n\n", target1, target2);
+
+    //ファイルを読み込みモードで開く
+    fp = fopen("../../dataset/normalTraining_log/modsec_audit.log","r");
+
+    //ファイルオープンに失敗した場合
+    if(fp == NULL){
+        printf("file open failed\n");
+        return -1;
+    }
+
+    while((fgets(str_buf,BUFSIZE,fp)) != NULL){
+
+        printf("%s", str_buf);
+
+        // 指定文字列での文字列検索
+        adr1 = strstr(str_buf, target1);
+        if(adr1 != NULL) counter1++;
+        printf("文字列の検索結果: %s\n", adr1); //test
+    
+        // 指定文字列での文字列検索(該当文字列なしの場合)
+        adr2 = strstr(str_buf, target2);
+        if(adr2 != NULL) counter2++;
+        printf("文字列の検索結果: %s\n", adr2); //test
+
+    }
+
+    //Result
+    printf("TARGET 1 : %d\n", counter1);
+    printf("TARGET 2 : %d\n", counter2);
+
+    //ファイルを閉じる
+    fclose(fp);
+
+    return 0;
+}
